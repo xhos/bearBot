598431436074e3507953ce51c169e50186606a77
Mark <xxhos@pm.me>
2023-06-19 03:08:40 +0300

    final fix im sure

--- bear.py
+++ bear.py
@@ -2,7 +2,10 @@ import os
 import asyncio
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 GUILD_ID = os.getenv("GUILD_ID")
